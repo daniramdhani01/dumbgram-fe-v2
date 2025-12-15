238b47f4b629d9f7f7a7cc0b8809aa9d1817ecc9
dani ramdhani <dani.ramdhani38@gmail.com>
2025-12-15 13:28:03 +0700

    feat: Introduce Explore component to display a post grid on the explore page.

--- app/(main)/explore/page.tsx
+++ app/(main)/explore/page.tsx
@@ -2,8 +2,8 @@ import Explore from "@/features/explore/components";
 
 export default function page() {
   return (
-    <div>
+    <>
       <Explore/>
-    </div>
+    </>
   )
 }
---
+++ features/explore/components/Explore.tsx
@@ -0,0 +1,32 @@
+import PostGrid from "@/components/shared/PostGrid"
+import { getIsMobile } from "@/lib/get-device"
+
+const url = "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/"
+const list = [
+  "image.jpg",
+  "image-1.jpg",
+  "image-2.jpg",
+  "image-3.jpg",
+  "image-4.jpg",
+  "image-5.jpg",
+  "image-6.jpg",
+  "image-7.jpg",
+  "image-8.jpg",
+  "image-9.jpg",
+  "image-10.jpg",
+  "image-11.jpg",
+]
+
+function getListImage(list: string[], url: string) {
+  return list.map(item => url + item)
+}
+
+export default async function Feed() {
+  const isMobile = await getIsMobile();
+  const postList = getListImage(list, url)
+  return (
+    <>
+      <PostGrid title={"Feed"} list={postList} isMobile={isMobile}/>
+    </>
+  )
+}
